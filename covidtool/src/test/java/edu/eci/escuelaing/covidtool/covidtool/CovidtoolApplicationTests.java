2f563ec5609becd96e43b4c4ff41c99d0d82cb12
Ricardo Amaya <ricardo.amaya@escuelaing.edu.co>
2021-10-28 07:44:31 -0500

    Estructura creada

---
+++ covidtool/src/test/java/edu/eci/escuelaing/covidtool/covidtool/CovidtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.escuelaing.covidtool.covidtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
